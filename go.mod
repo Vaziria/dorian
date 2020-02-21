4fb77a527507926082dfdad6397e8859a4cc8a34
kampretcode <manorder123@gmail.com>
2020-02-21 16:35:17 +0700

    test heroki

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/vaziria/dorian
+
+go 1.13
+
+require github.com/imroc/req v0.3.0
--- test.go
+++ test.go
@@ -4,8 +4,6 @@ import "fmt"
 import "net/http"
 import (
 	"os"
-	"log"
-	"github.com/imroc/req"
 )
 
 
@@ -23,20 +21,6 @@ func testRequest(w http.ResponseWriter, r *http.Request){
 }
 
 
-func handlingTest(w http.ResponseWriter, requ *http.Request){
-
-	var r = req.New()
-	var respon, err = r.Get("http://www.mocky.io/v2/5e4f810830000090002268f8")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var pesan string = string(respon.Body)
-
-	w.Write([]byte(pesan))
-
-}
 
 
 
@@ -51,7 +35,6 @@ func main(){
 	var address string = "0.0.0.0:" + port
 
 	http.HandleFunc("/", hanlderIndex)
-	http.HandleFunc("/blues", handlingTest)
 
 	fmt.Println(address)
 
